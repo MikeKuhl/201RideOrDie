9ec1730077b322ecf3b6ad437279600b853a6e9d
Emily Landers <emily.j.landers@gmail.com>
2021-11-16 14:03:22 -0500

    keypress

--- ghost.js
+++ ghost.js
@@ -1,40 +1,30 @@
 document.onkeydown = detectKey;
 let keyPress = 0;
+
 function detectKey(e) {
     let posLeft = document.getElementById('spooky').offsetLeft;
      e = e || window.event;
 
     if (e.keyCode == '37') {
          document.getElementById('spooky').style.marginLeft  = (posLeft+38)+"px";
-         keyPress++
+         keyPress -= 1
      }
     else if (e.keyCode == '39') {
         document.getElementById('spooky').style.marginLeft  = (posLeft-38)+"px";
-        keyPress
-    }
-  
+        keyPress++
+    } 
+    console.log(keyPress)
 }
-let posKnight = document.getElementById('knight').offsetRight;
-let posLeft = document.getElementById('spooky').offsetLeft;
 
-
-
-
-// function collision(){
-// while(posLeft === -646 + 'px'){
-// console.log('boom')
-//     }
-// }
-// function collision() {
-//     if(document.getElementById('spooky').style.marginLeft !== document.getElementById('knight').style.marginLeft){
-//         console.log('no')
-//     }
-//     else if(document.getElementById('spooky').style.marginLeft === document.getElementById('knight').style.marginLeft){
-//         console.log('boom')
+// function posPrompt(){
+//     if(keyPress == 16){
+//         console.log("working")
 //     }
 // }
 
-collision();
+// posPrompt();
+
+// collision();
 // // start button
 
 //const createBtn = (text = 'No text') => {
