52cba889036a51ff08610d45602446c5e7448d63
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-11-16 11:40:37 -0800

    question addition

--- ghost.js
+++ ghost.js
@@ -2,35 +2,34 @@ document.onkeydown = detectKey;
 let keyPress = 0;
 
 function detectKey(e) {
-    let posLeft = document.getElementById('spooky').offsetLeft;
-     e = e || window.event;
+  let posLeft = document.getElementById("spooky").offsetLeft;
+  e = e || window.event;
 
-    if (e.keyCode == '37') {
-         document.getElementById('spooky').style.marginLeft  = (posLeft+38)+"px";
-         keyPress -= 1
-     }
-    else if (e.keyCode == '39') {
-        document.getElementById('spooky').style.marginLeft  = (posLeft-38)+"px";
-        keyPress++
-    } 
-    console.log(keyPress)
-    posPrompt();
+  if (e.keyCode == "37") {
+    document.getElementById("spooky").style.marginLeft = posLeft + 38 + "px";
+    keyPress -= 1;
+  } else if (e.keyCode == "39") {
+    document.getElementById("spooky").style.marginLeft = posLeft - 38 + "px";
+    keyPress++;
+  }
+  console.log(keyPress);
+
+  posPrompt();
 }
 
-function posPrompt(){
-    if(keyPress == 16){
-        console.log("working")
-    }
+function posPrompt() {
+  if (keyPress == 16) {
+    console.log("working");
+    showQuestion(question);
+  }
 }
-
-
 
 // collision();
 // // start button
 
 //const createBtn = (text = 'No text') => {
 // const btn = document.createElement('button');
-  //  BigInt.innterText = text;
-  //  document.body.appendChild(btn);
-  //  return btn;  
+//  BigInt.innterText = text;
+//  document.body.appendChild(btn);
+//  return btn;
 // }
