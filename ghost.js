27082816ea1b5d66f3eced05328ead3438259fe0
Emily Landers <emily.j.landers@gmail.com>
2021-11-16 15:41:00 -0500

    button js

--- ghost.js
+++ ghost.js
@@ -1,4 +1,4 @@
-document.onkeydown = detectKey;
+
 let keyPress = 0;
 
 function detectKey(e) {
@@ -20,19 +20,24 @@ function detectKey(e) {
 
 function change(){
     let btn = document.getElementById("startButton");
-    if (btn.value=="Enter the Dungeon"){ 
+    document.onkeydown = detectKey;
+    if (btn.value == "Enter the Dungeon"){ 
         btn.value = "Exit the Dungeon"
     }
-    else btn.value = "Exit the Dungeon";
+    else {
+        stop();
+    }
     }
 
     function stop(){
     let btn = document.getElementById("startButton");
-        if (btn.value=="Exit the Dungeon"){ 
+        if (btn.value == "Exit the Dungeon"){ 
             btn.value = "Enter the Dungeon"
         }
-        else btn.value = "Enter the Dungeon";
+        else {
+            btn.value = "Enter the Dungeon";
         }
+    }
         
   
 
@@ -43,12 +48,4 @@ function posPrompt() {
   }
 }
 
-// collision();
-// // start button
 
-//const createBtn = (text = 'No text') => {
-// const btn = document.createElement('button');
-//  BigInt.innterText = text;
-//  document.body.appendChild(btn);
-//  return btn;
-// }
