34f9430ba7567f45e1fefe874a39e5767899b639
Emily Landers <emily.j.landers@gmail.com>
2021-11-16 13:30:51 -0500

    collision

--- ghost.js
+++ ghost.js
@@ -1,28 +1,38 @@
 document.onkeydown = detectKey;
-
+let keyPress = 0;
 function detectKey(e) {
     let posLeft = document.getElementById('spooky').offsetLeft;
      e = e || window.event;
 
     if (e.keyCode == '37') {
          document.getElementById('spooky').style.marginLeft  = (posLeft+38)+"px";
+         keyPress++
      }
     else if (e.keyCode == '39') {
         document.getElementById('spooky').style.marginLeft  = (posLeft-38)+"px";
+        keyPress
     }
   
 }
-let posKnight = document.getElementById('knight');
+let posKnight = document.getElementById('knight').offsetRight;
 let posLeft = document.getElementById('spooky').offsetLeft;
 
-function collision() {
-    if(document.getElementById('spooky').style.marginLeft !== document.getElementById('knight').style.marginLeft){
-        console.log('no')
-    }
-    else if(document.getElementById('spooky').style.marginLeft === document.getElementById('knight').style.marginLeft){
-        console.log('boom')
-    }
-}
+
+
+
+// function collision(){
+// while(posLeft === -646 + 'px'){
+// console.log('boom')
+//     }
+// }
+// function collision() {
+//     if(document.getElementById('spooky').style.marginLeft !== document.getElementById('knight').style.marginLeft){
+//         console.log('no')
+//     }
+//     else if(document.getElementById('spooky').style.marginLeft === document.getElementById('knight').style.marginLeft){
+//         console.log('boom')
+//     }
+// }
 
 collision();
 // // start button
