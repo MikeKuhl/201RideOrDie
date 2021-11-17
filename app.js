4c2789838ca4d7e8677a938c2580ce26f231d585
Emily Landers <emily.j.landers@gmail.com>
2021-11-16 22:07:06 -0500

    josh questions completed

--- app.js
+++ app.js
@@ -22,8 +22,47 @@ let question3 = {
 
 let question4 = {
   question:
-  'The symbol used for the assignment operator is?'
-}
+    'The symbol used for the assignment operator is?":',
+  answers: ["-", "+", "=", ">"],
+  correctAnswer: 1,
+};
+let question5 = {
+  question:
+    'Which is not a JavaScript variable?":',
+  answers: ["let", "net", "var", "const"],
+  correctAnswer: 1,
+};
+let question6 = {
+  question:
+    'Hyper Text Markup Language Stands For?":',
+  answers: ["JavaScript", "CSS", "Java", "HTML"],
+  correctAnswer: 3,
+};
+let question7 = {
+  question:
+    'In JavaScript can you use a number as an identifer (name)":',
+  answers: ["True", "False"],
+  correctAnswer: 1,
+};
+let question8 = {
+  question:
+    'Does a boolean only do true statements?":',
+  answers: ["True", "False", "Both", "Truthy"],
+  correctAnswer: 2,
+};
+let question9 = {
+  question:
+    'What is a NaN property in JavaScript?":',
+  answers: ["Not-a-Nemo", "Not-a-Number", "Nor-a-Number", "Number-a-Note"],
+  correctAnswer: 0,
+};
+let question10 = {
+  question:
+    'What symbol does an array use?":',
+  answers: ["()", "{}", "[]", "<>"],
+  correctAnswer: 2,
+};
+
 
 function showQuestion(q) {
   let questionDiv = document.getElementById("question");
