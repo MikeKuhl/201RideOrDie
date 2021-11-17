9df1d66c0b8dff1ac54467c6e538978ea6d7ca9e
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-11-17 12:15:49 -0800

    refactor

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //questions
 
-
 let question = {
   question: "The 'function' and 'var' are known as:",
   answers: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
@@ -22,68 +21,59 @@ let question3 = {
 };
 
 let question4 = {
-  question:
-    'The symbol used for the assignment operator is?":',
+  question: 'The symbol used for the assignment operator is?":',
   answers: ["-", "+", "=", ">"],
   correctAnswer: 1,
 };
 let question5 = {
-  question:
-    'Which is not a JavaScript variable?":',
+  question: 'Which is not a JavaScript variable?":',
   answers: ["let", "net", "var", "const"],
   correctAnswer: 1,
 };
 let question6 = {
-  question:
-    'Hyper Text Markup Language Stands For?":',
+  question: 'Hyper Text Markup Language Stands For?":',
   answers: ["JavaScript", "CSS", "Java", "HTML"],
   correctAnswer: 3,
 };
 let question7 = {
-  question:
-    'In JavaScript can you use a number as an identifier (name)":',
+  question: 'In JavaScript can you use a number as an identifier (name)":',
   answers: ["True", "False"],
   correctAnswer: 1,
 };
 let question8 = {
-  question:
-    'Does a boolean only do true statements?":',
+  question: 'Does a boolean only do true statements?":',
   answers: ["True", "False", "Both", "Truthy"],
   correctAnswer: 2,
 };
 let question9 = {
-  question:
-    'What is a NaN property in JavaScript?":',
+  question: 'What is a NaN property in JavaScript?":',
   answers: ["Not-a-Nemo", "Not-a-Number", "Nor-a-Number", "Number-a-Note"],
   correctAnswer: 0,
 };
 let question10 = {
-  question:
-    'What symbol does an array use?":',
+  question: 'What symbol does an array use?":',
   answers: ["()", "{}", "[]", "<>"],
   correctAnswer: 2,
 };
 
-
 function showQuestion(q) {
   let questionDiv = document.getElementById("question");
   questionDiv.textContent = q.question;
- let alts = document.querySelectorAll(".answers");
+  let answers = document.querySelectorAll(".answers");
 
-  alts.forEach(function (element, index) {
+  answers.forEach(function (element, index) {
     element.textContent = q.answers[index];
 
     element.addEventListener("click", function () {
       if (q.correctAnswer == index) {
         canMove = true;
-        document.getElementById('knight').hidden = true;
+        document.getElementById("knight").hidden = true;
         // console.log("Correct Answer!");
       } else {
         console.log("Wrong Answer!");
-        lives -=1
-        livesCheck(); 
+        lives -= 1;
+        livesCheck();
         console.log(lives);
-
       }
     });
   });
