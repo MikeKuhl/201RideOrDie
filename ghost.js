aa3d1aa793c678db451606e37a583407592bb85e
Emily Landers <emily.j.landers@gmail.com>
2021-11-18 19:04:45 -0500

    final

--- ghost.js
+++ ghost.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 522b1f93239f01fe1818b86edac5441bbebdabf0
 let canMove = true;
 let keyPress = 0;
 let lives = 4;
@@ -21,11 +18,11 @@ function Questions(question, answers, correctAnswer){
   
   let question3 = new Questions('Choose the correct snippet from the following to check if the variable "a" is not equal the "NULL":',["if(a!==null)", "if (a!)", "if(a!null)", "if(a!=null)"],0)
   
-  let question4 = new Questions('The symbol used for the assignment operator is?":',["-", "+", "=", ">"], 1,)
+  let question4 = new Questions('The symbol used for the assignment operator is?":',["-", "+", "=", ">"], 2,)
 
   let question5 = new Questions('Which is not a JavaScript variable?":', ["let", "net", "var", "const"], 1)
 
-  let question6 = new Questions('Hyper Text Markup Language Stands For?":', ["JavaScript", "CSS", "Java", "HTML"], 3,)
+  let question6 = new Questions('What is the abbreviation for Hyper Text Markup Language?":', ["JavaScript", "CSS", "Java", "HTML"], 3,)
    
   let question7 = new Questions('In JavaScript can you use a number as an identifier (name)":',["True", "False"], 1,)
 
@@ -61,9 +58,11 @@ function handleClick(event) {
 
   console.log("target id" , event.target.id)
   if(event.target.id === 'correct'){ 
+    
     canMove = true;
     ask.innerHTML = ""
   } else if (event.target.id !== 'correct') {
+    livesCheck();
     canMove = false;
     lives -= 1;
     console.log(lives)
@@ -80,18 +79,19 @@ function detectKey(e) {
     e = e || window.event;
    
     if (e.keyCode == "37") {
-      document.getElementById("spooky").style.marginLeft = posLeft + 38 + "px";
+      document.getElementById("spooky").style.marginLeft = posLeft + 41 + "px";
       keyPress -= 1;
     } else if (e.keyCode == "39") {
-      document.getElementById("spooky").style.marginLeft = posLeft - 38 + "px";
+      document.getElementById("spooky").style.marginLeft = posLeft - 41 + "px";
       keyPress++;
     }
+    
     posPrompt();
   }
 }
 
 
-const hearts = [
+let hearts = [
   document.getElementById("heart1"),
   document.getElementById("heart2"),
   document.getElementById("heart3"),
@@ -99,42 +99,42 @@ const hearts = [
 ];
 
 function livesCheck() {
-  for (i = 0; i < hearts.length; i++) {
-    if (lives === 3) {
+  // for (i = 0; i < hearts.length; i++) {
+    if (lives === 4) {
       hearts[0].remove();
-    } else if (lives === 2) {
+    } else if (lives === 3) {
       hearts[1].remove();
-    } else if (lives === 1) {
+    } else if (lives === 2) {
       hearts[2].remove();
-    } else if (lives === 0) {
+    } else if (lives === 1) {
       hearts[3].remove();
       document.getElementById("gameover").hidden = false;
     } else {
       console.log("nice");
     }
   }
-}
+
 
 function posPrompt() {
   let hotbod = document.getElementById("hotbod");
-  if (keyPress === 21) {
+  if (keyPress === 15) {
     console.log("working");
     question1.render();
     hotbod.style.overflow = "hidden";
     document.getElementById("knight").hidden = false;
     canMove = false;
-  } else if (keyPress > 21) {
+  } else if (keyPress > 15) {
     document.getElementById("knight").hidden = true;
     canMove = true;
     hotbod.style.overflow = "auto";
-  } else if (keyPress < 21) {
+  } else if (keyPress < 15) {
     document.getElementById("knight").hidden = true;
     canMove = true;
     hotbod.style.overflow = "auto";
   } else {
     canMove = true;
     hotbod.style.overflow = "auto";
-  } if (keyPress === 59) {
+  } if (keyPress === 50) {
     console.log("working");
     document.getElementById("knight").hidden = false;
     document.getElementById("knight").style.marginLeft = 1450 + 'px';
@@ -143,7 +143,7 @@ function posPrompt() {
     
     canMove = false;
   }
-  if (keyPress === 98) {
+  if (keyPress === 85) {
     console.log("working");
     question3.render();
     hotbod.style.overflow = "hidden";
@@ -151,20 +151,60 @@ function posPrompt() {
     document.getElementById("knight").style.marginLeft = 2820 + 'px';
     canMove = false;
   } 
-  if (keyPress === 147) {
+  if (keyPress === 120) {
     console.log("working");
     question4.render();
     hotbod.style.overflow = "hidden";
     document.getElementById("knight").hidden = false;
-    document.getElementById("knight").style.marginLeft =  + 4794 + 'px';
+    document.getElementById("knight").style.marginLeft =  + 4300 + 'px';
     canMove = false;
   }
-  if (keyPress === 191) {
+  if (keyPress === 155) {
     console.log("working");
     question5.render();
     hotbod.style.overflow = "hidden";
     document.getElementById("knight").hidden = false;
-    document.getElementById("knight").style.marginLeft =  + 6466 + 'px';
+    document.getElementById("knight").style.marginLeft =  + 5695 + 'px';
+    canMove = false;
+  }
+  if (keyPress === 190) {
+    console.log("working");
+    question6.render();
+    hotbod.style.overflow = "hidden";
+    document.getElementById("knight").hidden = false;
+    document.getElementById("knight").style.marginLeft =  + 7180 + 'px';
+    canMove = false;
+  }
+  if (keyPress === 225) {
+    console.log("working");
+    question7.render();
+    hotbod.style.overflow = "hidden";
+    document.getElementById("knight").hidden = false;
+    document.getElementById("knight").style.marginLeft =  + 8600 + 'px';
+    canMove = false;
+  }
+  if (keyPress === 261) {
+    console.log("working");
+    question8.render();
+    hotbod.style.overflow = "hidden";
+    document.getElementById("knight").hidden = false;
+    document.getElementById("knight").style.marginLeft =  + 8675 + 'px';
+    canMove = false;
+  }
+  if (keyPress === 296) {
+    console.log("working");
+    question9.render();
+    hotbod.style.overflow = "hidden";
+    document.getElementById("knight").hidden = false;
+    document.getElementById("knight").style.marginLeft =  + 10151 + 'px';
+    canMove = false;
+  }
+  if (keyPress === 331) {
+    console.log("working");
+    question10.render();
+    hotbod.style.overflow = "hidden";
+    document.getElementById("knight").hidden = false;
+    document.getElementById("knight").style.marginLeft =  + 11621 + 'px';
     canMove = false;
   }
 }
@@ -172,4 +212,4 @@ function setUp(){
   let ask = document.getElementById('quest-container');
   ask.addEventListener('click' , handleClick);
 }
-setUp();
+setUp()
